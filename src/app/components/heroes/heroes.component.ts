33df3cbb3e6542bd7b1ffe4b567045cb5cf7edd1
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-10-12 15:57:02 -0500

    Heroe button link added to HeroesComponentHtml for get Heroe info

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeroesComponent implements OnInit {
+  
+  heroes:Heroe[] = [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(index: number) {
+    this.router.navigate(['/heroe', index]);
   }
 
 }
