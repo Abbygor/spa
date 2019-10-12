ad58067c3f6c09a058d1a83aeda2da9bdbe98c7e
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-10-12 15:55:14 -0500

    Heroe component added and its route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { APP_ROUTING } from './app.routes';
+import { HeroesService } from './services/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,15 @@ import { APP_ROUTING } from './app.routes';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
   providers: [
+    HeroesService
   ],
   bootstrap: [AppComponent]
 })
